33f98fad26e1f69a7caeed25d97fcbdce42845e6
taishikato <taishi.k0903@gmail.com>
2024-07-12 17:57:14 +0900

    Update text in Home component for consistency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,11 +14,12 @@ export default function Home() {
         />
         <h1 className="w-[90%] md:w-2/5 mt-3">
           <span className="mb-5 block">
-            I'm <span className="text-secondary-foreground">Taishi</span>, a
-            fullstack developer and solofounder based in Japan and Canada.
+            I&apos;m <span className="text-secondary-foreground">Taishi</span>,
+            a fullstack developer and solofounder based in Japan and Canada.
           </span>
           <p>
-            I've built and shipped AI/non-AI products (with over 18K users).
+            I&apos;ve built and shipped AI/non-AI products (with over 18K
+            users).
           </p>
         </h1>
       </div>
