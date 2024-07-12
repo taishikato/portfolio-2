34f74b7112ea20b499b642f827264cf03acb8392
taishikato <taishi.k0903@gmail.com>
2024-07-12 12:04:46 +0900

    Add profile image to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,25 @@
 import { MyWork } from "@/components/my-work";
+import ProfileImage from "../assets/profile.webp";
 
 export default function Home() {
   return (
     <div className="space-y-10">
-      <h1 className="w-[90%] md:w-2/5">
-        <span className="mb-5 block">
-          I'm <span className="text-secondary-foreground">Taishi</span>, a
-          fullstack developer and solofounder based in Japan and Canada.
-        </span>
-        <p>I've built and shipped AI/non-AI products (with over 18K users).</p>
-      </h1>
+      <div>
+        <img
+          src={ProfileImage.src}
+          className="w-14 rounded-3xl"
+          alt="profile image"
+        />
+        <h1 className="w-[90%] md:w-2/5 mt-3">
+          <span className="mb-5 block">
+            I'm <span className="text-secondary-foreground">Taishi</span>, a
+            fullstack developer and solofounder based in Japan and Canada.
+          </span>
+          <p>
+            I've built and shipped AI/non-AI products (with over 18K users).
+          </p>
+        </h1>
+      </div>
 
       <MyWork />
     </div>
