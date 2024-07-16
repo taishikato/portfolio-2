835826d80da8e127de7f9c253754e25c5ece1f2f
taishikato <taishi.k0903@gmail.com>
2024-07-16 09:50:00 +0900

    Update footer padding to py-8

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,6 +1,6 @@
 export const Footer = () => {
   return (
-    <footer className="text-xs border-t py-6 mt-8">
+    <footer className="text-xs border-t py-8 mt-8">
       Taishi @ {new Date().getFullYear()}
     </footer>
   );
