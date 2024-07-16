f81cedb68de57a5440f618ae0813622664cbe52e
taishikato <taishi.k0903@gmail.com>
2024-07-16 09:26:45 +0900

    call airtable api

--- src/components/my-work.tsx
+++ src/components/my-work.tsx
@@ -1,4 +1,18 @@
-export const MyWork = () => {
+export const MyWork = async () => {
+  const res = await fetch(
+    "https://api.airtable.com/v0/appIrznU5cXdfKBMF/tblBRMPviarGbkF1y",
+    {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${process.env.AIRTABLE_API_KEY}`,
+      },
+    }
+  );
+
+  const json = await res.json();
+
+  console.log({ json });
+
   return (
     <div>
       <h2 className="text-secondary-foreground w-full border-b pb-3 mb-7 font-semibold">
