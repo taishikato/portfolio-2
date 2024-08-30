cde10dd7f8d4609a72ff467aac794b069976eea3
taishikato <taishi.k0903@gmail.com>
2024-08-30 16:19:05 +0900

    add stack items

--- src/app/stack/page.tsx
+++ src/app/stack/page.tsx
@@ -1,12 +1,30 @@
 import Link from "next/link";
-import imageAer from "../../assets/stack/aer.webp";
 import { MoveRight } from "lucide-react";
+import imageAer from "../../assets/stack/aer.webp";
+import imagemacbookAir from "../../assets/stack/mackbook-air.png";
+import imageAirpodsPro2 from "../../assets/stack/airpods-pro2.webp";
+import imageAppleWatch6 from "../../assets/stack/apple-watch-6.webp";
 
 const stacks = [
   {
     title: "Travel Pack 3 Small X-Pac",
     image: imageAer,
     link: "https://aersf.com/collections/backpacks/products/travel-pack-3-small-x-pac",
+  },
+  {
+    title: "MacBook Air (M1, 2020)",
+    image: imagemacbookAir,
+    link: "https://support.apple.com/en-us/111883",
+  },
+  {
+    title: "AirPods Pro (2nd generation)",
+    image: imageAirpodsPro2,
+    link: "https://www.apple.com/airpods-pro/",
+  },
+  {
+    title: "Apple Watch Series 6",
+    image: imageAppleWatch6,
+    link: "https://support.apple.com/en-us/111918",
   },
 ];
 
@@ -27,19 +45,22 @@ export default function StackPage() {
           Stack
         </Link>
       </div>
-      <ul className="flex gap-x-5">
+      <ul className="flex gap-5 flex-wrap">
         {stacks.map((stack) => {
           const { title, image, link } = stack;
 
           return (
-            <li key={stack.title} className="w-full max-w-[300px]">
+            <li
+              key={stack.title}
+              className="w-1/2 max-w-1/2 flex-1 md:w-full md:max-w-[300px]"
+            >
               <a href={link} target="_blank">
                 <img
                   src={image.src}
                   alt={title}
                   width={image.width}
                   height={image.height}
-                  className="rounded-xl mb-3"
+                  className="rounded-xl mb-3 w-full"
                 />
               </a>
               <div className="text-secondary-foreground mb-5">{title}</div>
